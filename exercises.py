f8a8792f399edff8ceda77a932b4ef930ae24fa6
Brandon Cohen <brobcohen@gmail.com>
2020-10-17 14:48:26 +0200

    implement multiples function

--- exercises.py
+++ exercises.py
@@ -6,5 +6,24 @@ def factorial(num):
     pass
 
 
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
 def multiples(numbers, n):
-    pass
+    
+    new_list = []
+    
+    x = 1
+    
+    while x in numbers:
+        if n % x == 0:
+            new_list.append(str(x))
+        x += 1
+    
+    print(new_list)
+ 
+    
+    
+    return
+# current problem -- can't use numbers as a list, needs integer
